b237ab7a7187a7824ae50c4cf2757c075fb1feb3
ebey <007ebey@gmail.com>
2026-08-06 18:07:52 +0530

    build

--- prayer-api/internal/http/auth_handler_test.go
+++ prayer-api/internal/http/auth_handler_test.go
@@ -50,11 +50,11 @@ func authenticatedRequest(externalID string) *http.Request {
 		nil,
 	)
 
-	claims := &clerk.SessionClaims{
-		RegisteredClaims: jwt.RegisteredClaims{
-			Subject: externalID,
-		},
-	}
+    claims := &clerk.SessionClaims{
+	  RegisteredClaims: clerk.RegisteredClaims{
+		Subject: externalID,
+	 },
+    }
 
 	ctx := clerk.ContextWithSessionClaims(
 		req.Context(),
