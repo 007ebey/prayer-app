6466617fadfc0d90bd47819cb5a608e8a50c29bb
ebey <007ebey@gmail.com>
2026-08-14 11:59:21 +0530

    progress

--- prayer-api/internal/domain/role/errors.go
+++ prayer-api/internal/domain/role/errors.go
@@ -6,4 +6,5 @@ var (
 	ErrNameRequired       = errors.New("role name is required")
 	ErrInvalidPermission  = errors.New("invalid permission")
 	ErrPermissionRequired = errors.New("at least one permission is required")
+	ErrNotFound 		  = errors.New("role not found")
 )
